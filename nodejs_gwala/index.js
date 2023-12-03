4a0db96af9e4d584bfdd3e249bf50b9959421824
DrNajim <najimabdessamad.na@gmail.com>
2023-12-03 12:24:09 +0100

    add dockerfile

--- nodejs_gwala/index.js
+++ nodejs_gwala/index.js
@@ -15,7 +15,10 @@ const token = require("./modals/token");
 const PORT=process.env.PORT1 || process.env.PORT2;
 const URI= process.env.MONGODB_URI;
 app.listen(PORT, () => console.log(`server running on port ${PORT}...`));
-mongoose.connect(`${URI}`);
+mongoose.connect(`${URI}`, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(error => console.error('Error connecting to MongoDB:', error));
+
 app.use(cors());
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
